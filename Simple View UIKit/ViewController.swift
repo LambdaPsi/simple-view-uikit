b38a3e6d7d7a9ad6c9df2d253fb4b3ea258b0543
Sam <samuel.nentwig@hotmail.com>
2023-01-16 13:29:29 +0100

    Initial Commit

---
+++ Simple View UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple View UIKit
+//
+//  Created by Samuel Nentwig on 16.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
